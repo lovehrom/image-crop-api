1758da14a5d42f3521e854acb0cabe5284d609bf
Shut <kostianchik@list.ru>
2026-02-23 16:49:02 +0500

    Fix crop.js export for Vercel root function

--- crop.js
+++ crop.js
@@ -1,7 +1,7 @@
 const sharp = require('sharp');
 const formidable = require('formidable');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
